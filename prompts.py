812413169314863f030e4411f16fee14ec80c22e
kapil-85 <168553846+kapil-85@users.noreply.github.com>
2024-07-08 17:06:32 +0530

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -138,9 +138,9 @@ ai_topic_prompt1="""Based on the context, generate only questions considering fo
     5. type of questions - {}
     6. Level of questions - {}
     questions and answers should be started from new line.
-    Response is to be generated in both english and Hindi,first generate in english then in Hindi
-    and if you are generating Answers also, Answer it in a new line and Generate questions according to selected Pre Uploaded Document only, Don't take help from the internet.
-    If the Topic is not present in the selected Document, write "This topic is not present in Book" & then list all the topics which are present in the uploaded document in tabular format and call this list "Topics available"
+    Response is to be generated in both english and Hindi,first generate in english then in Hindi.
+    If Answers are required, always start the Answer in a new line.Generate questions according to selected Pre Uploaded Document only, Don't take help from the internet.
+    If the Topic is not present in the selected Document, don't provide any questions or answers and rather write "This topic is not present in Book" & then list all the topics which are present in the uploaded document in tabular format and call this list "Topics available"
     
 """
 ai_topic_prompt_questions="""Based on the context, extract only questions
