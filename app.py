b8cba97b7d0399c271366228cd86a8f26aec29d4
kapil-85 <168553846+kapil-85@users.noreply.github.com>
2024-07-09 15:20:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -489,7 +489,7 @@ if st.session_state.teach=='Teachers':
                             with col2:
                                 st.session_state.topic_name = st.text_input('Specific chapter/topic name*',placeholder="AI Chapter/Topic Name")
                                 st.session_state.type_of_questions =  st.selectbox('Choose question type*', ['Short Questions', 'Long Questions','MCQ','Fill in the Blanks','True and False'],index=0)
-                                st.session_state.language =  st.selectbox('Choose response language mode*', ['English and Hindi'],index=0,key="lang")
+                                st.session_state.language =  st.selectbox('Choose response language mode*', ['English','English and Hindi'],index=0,key="lang")
                             # docsearch = chat.create_doc_embeddings(documents)
                             #if is_word_in_text(st.session_state.topic_name,formatted_output) or st.session_state.topic_name=='' :
                             
