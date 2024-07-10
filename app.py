6aa2f072bf0b8297cb27eaf5665c9f19fc588069
kapil-85 <168553846+kapil-85@users.noreply.github.com>
2024-07-10 08:48:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -335,13 +335,14 @@ if st.session_state.teach=='Teachers':
                                     print("============================")
                                     print(CHECK_PROMPT)
                                     print("============================")
-                                    print("============CHECKING================")
-                                    formatted_output = st.session_state.llm.predict(input = ai_topic_prompt1.format(st.session_state.topic_name,
-                                                                                                                           st.session_state.no_of_questions,
-                                                                                                                           st.session_state.mode_of_questions,
+                                    print("============CHECKING================") #st.session_state.mode_of_questions
+                                    formatted_output = st.session_state.llm.predict(input = ai_topic_prompt1.format(st.session_state.no_of_questions,
                                                                                                                            st.session_state.type_of_questions,
                                                                                                                            st.session_state.complexity,
-                                                                                                                           st.session_state.text))
+                                                                                                                           st.session_state.topic_name,
+                                                                                                                           st.session_state.text
+                                                                                                                   )
+                                                                                   )
                                     st.info(formatted_output)
                                     markdown_to_pdf(formatted_output,'question.pdf')
                     # Storing the chat
